f9f54afd2570c2a2996857f97e9d23cd5978ea33
Shahab Khalvati <contact@shahabkhalvati.com>
2019-12-18 22:19:49 +0100

    aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,8 @@ alias ls='ls -lhGF'
 alias run-all-apps='sudo spctl --master-disable'
 alias run-all-apps-revert='sudo spctl --master-enable'
 alias utm="youtube-dl --download-archive ~/Documents/appdata/videoarchive.txt -f 'best[height<=480]'"
-alias uth="youtube-dl --download-archive ~/Documents/appdata/videoarchive.txt"
+alias uth="youtube-dl --download-archive ~/Documents/appdata/videoarchive.txt -f 'best[height<=720]'"
 alias ut='youtube-dl --download-archive ~/Documents/appdata/videoarchive.txt'
+alias eggt='youtube-dl -o "%(playlist_index)s-%(title)s.%(ext)s"'
+alias egglist='youtube-dl -o "%(playlist)s/%(playlist_index)s-%(title)s.%(ext)s" --batch-file '
 alias docker_clear='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
